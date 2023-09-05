f575c0230dae4d2c5bff09113e88b19b25b9c5c0
VittorTrigone <Vittor.Trigone.Dias@gmail.com>
2023-09-05 16:50:56 -0300

    Arrumando coisas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,19 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
+import {LOCALE_ID} from '@angular/core';
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePt);
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
+    {provide: LOCALE_ID, useValue: "pt-BR"}
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
